58e6d911856c7f36ef1f2c58b72b01e6c83c1723
AIMEE GARRIOCK <AIMEEGARRIOCK@gmail.com>
2021-04-14 16:06:15 -0700

    resetting

---
+++ src/components/DayList.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import DayListItem from "components/DayListItem";
+
+export default function DayList(props){
+  const days = props.days;
+
+  const handleDayList = days.map(day => <DayListItem 
+    key={day.id}
+    name={day.name}
+    spots={day.spots} 
+    selected={day.name === props.day}
+    setDay={props.setDay}
+    />);
+  return <ul>
+    { handleDayList }
+  </ul>
+}
+
+<DayList days={days} day={day} setDay={setDay} />
